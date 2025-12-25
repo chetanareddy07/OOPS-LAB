adfc2b41e5d85544f7d192b23d293c92510ab0d2
chetanareddy07 <narreddychetanareddy@gmail.com>
2025-12-25 09:37:37 +0530

    Add files via upload

---
+++ Lab_1/Largest.java
@@ -0,0 +1,11 @@
+class Largest {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+
+        if (a > b)
+            System.out.println("a is largest");
+        else
+            System.out.println("b is largest");
+    }
+}
