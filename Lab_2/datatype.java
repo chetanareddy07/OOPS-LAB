158d779633ec6a0947ae4adb4378e00a9a357ed4
chetanareddy07 <narreddychetanareddy@gmail.com>
2026-01-19 20:06:38 +0530

    Add files via upload

---
+++ Lab_2/datatype.java
@@ -0,0 +1,15 @@
+public class datatype{
+public static void main(String[] args){
+int items = 50;
+float costperitem = 9.66f;
+float cost = items*costperitem;
+char currency = '$';
+System.out.println("Number of items:" + items);
+System.out.println("Cost of one item:" + costperitem + currency);
+System.out.println("Total cost:" + cost + currency);
+}
+}
+
+
+
+
